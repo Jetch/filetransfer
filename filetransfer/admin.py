4b3b460f862a4180106bfeb41087bc1895f745f0
Juho <4092828+Jetch@users.noreply.github.com>
2026-01-19 13:12:55 +0200

    Updated User model in admin site

--- filetransfer/admin.py
+++ filetransfer/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User, Organization, File, Download
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 admin.site.register(Organization)
 admin.site.register(File)
 admin.site.register(Download)
