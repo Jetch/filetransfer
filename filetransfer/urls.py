23252e0a2a19a8a7718aa4753b5059014be57faa
Juho <4092828+Jetch@users.noreply.github.com>
2026-01-19 16:56:34 +0200

    Added serializers, views and urls

---
+++ filetransfer/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("files/upload/", views.upload_file, name="upload-file"),
+    path("files/", views.list_files, name="list-files"),
+    path("files/<int:file_id>/download/", views.download_file, name="download-file"),
+    path("files/<int:file_id>/downloads/", views.file_downloads, name="file-downloads"),
+    path("organizations/", views.list_organizations, name="list_organizations"),
+    path("user/<int:user_id>/downloads/", views.user_downloads, name="user-downloads"),
+]
