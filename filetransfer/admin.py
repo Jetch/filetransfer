b43f6737d4e857aed67df28e4e5d9a0cc8ff6b88
Juho <4092828+Jetch@users.noreply.github.com>
2026-01-19 13:03:54 +0200

    Added Organization, User, File and Upload models

--- filetransfer/admin.py
+++ filetransfer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Organization, File, Download
 
+admin.site.register(User, UserAdmin)
+admin.site.register(Organization)
+admin.site.register(File)
+admin.site.register(Download)
 # Register your models here.
