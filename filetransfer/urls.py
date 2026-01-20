89dd654bb67aeac1311d49a833be6230fb161c64
Juho <4092828+Jetch@users.noreply.github.com>
2026-01-20 14:12:45 +0200

    Fixed urlspatterns, user-download and list-organization

--- filetransfer/urls.py
+++ filetransfer/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("files/", views.list_files, name="list-files"),
     path("files/<int:file_id>/download/", views.download_file, name="download-file"),
     path("files/<int:file_id>/downloads/", views.file_downloads, name="file-downloads"),
-    path("organizations/", views.list_organizations, name="list_organizations"),
-    path("user/<int:user_id>/downloads/", views.user_downloads, name="user-downloads"),
+    path("organizations/", views.list_organizations, name="list-organizations"),
+    path("users/<int:user_id>/downloads/", views.user_downloads, name="user-downloads"),
 ]
